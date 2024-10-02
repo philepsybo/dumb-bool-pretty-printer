e6b5f5b1065554f0eae14aeb5c8449ae7c2ac710
Philipp Bous <pb@leibniz-psychology.org>
2024-10-02 11:48:21 +0200

    REFACTORES sanitizer to be more readable

--- src/sanitizer.js
+++ src/sanitizer.js
@@ -1,21 +1,50 @@
 import { logError } from 'logger';
 
 export function checkSanity(tokens) {
-    let isSane = true;
     const parentheses = tokens.filter((token) => token.type === 'openParenthesis' || token.type === 'closeParenthesis');
+    const brackets = tokens.filter((token) => token.type === 'openBracket' || token.type === 'closeBracket');
+    const curlyBraces = tokens.filter((token) => token.type === 'openCurlyBrace' || token.type === 'closeCurlyBrace');
+
+    const conditionalTokens = tokens.filter((token) => token.type === 'conditionalIf' || token.type === 'conditionalThen');
+
+    const parenthesesCountMatches = checkParenthesesCountMatches(parentheses);
+    let parenthesesOrderingIsOk = true;
+    if (parenthesesCountMatches) {
+        parenthesesOrderingIsOk = checkParenthesesOrdering(parentheses);
+    }
+
+    const bracketCountMatches = checkBracketCountMatches(brackets);
+    let bracketOrderingIsOk = true;
+    if (bracketCountMatches) {
+        bracketOrderingIsOk = checkBracketOrdering(brackets);
+    }
+
+    const curlyBraceCountMatches = checkCurlyBraceCountMatches(curlyBraces);
+    let curlyBraceOrderingIsOk = true;
+    if (curlyBraceCountMatches) {
+        curlyBraceOrderingIsOk = checkCurlyBraceOrdering(curlyBraces);
+    }
+
+    const conditionalCountMatches = checkConditionalCountMatches(conditionalTokens);
+    const onlyLiteralsBetweenCurlyBraces = checkOnlyLiteralsBetweenCurlyBraces(tokens);
+    const onlyLiteralsBetweenBrackets = checkOnlyLiteralsBetweenBrackets(tokens);
+
+    return (
+        parenthesesCountMatches
+        && parenthesesOrderingIsOk
+        && bracketCountMatches
+        && bracketOrderingIsOk
+        && curlyBraceCountMatches
+        && curlyBraceOrderingIsOk
+        && conditionalCountMatches
+        && onlyLiteralsBetweenCurlyBraces
+        && onlyLiteralsBetweenBrackets
+    );
+}
+
+function checkParenthesesCountMatches(parentheses) {
     const openParenthesesCount = parentheses.filter((token) => token.type === 'openParenthesis').length;
     const closeParenthesesCount = parentheses.filter((token) => token.type === 'closeParenthesis').length;
-
-    const brackets = tokens.filter((token) => token.type === 'openBracket' || token.type === 'closeBracket');
-    const openBracketCount = brackets.filter((token) => token.type === 'openBracket').length;
-    const closeBracketCount = brackets.filter((token) => token.type === 'closeBracket').length;
-
-    const curlyBraces = tokens.filter((token) => token.type === 'openCurlyBrace' || token.type === 'closeCurlyBrace');
-    const openCurlyBraceCount = curlyBraces.filter((token) => token.type === 'openCurlyBrace').length;
-    const closeCurlyBracesCount = curlyBraces.filter((token) => token.type === 'closeCurlyBrace').length;
-
-    const conditionalIfCount = tokens.filter((token) => token.type === 'conditionalIf').length;
-    const conditionalThenCount = tokens.filter((token) => token.type === 'conditionalThen').length;
 
     if (openParenthesesCount !== closeParenthesesCount) {
         logError('Mismatched parentheses ()');
@@ -25,26 +54,35 @@ export function checkSanity(tokens) {
         if (closeParenthesesCount > openParenthesesCount) {
             logError(`Extra ${closeParenthesesCount - openParenthesesCount} closing parenthesis(es)`);
         }
-        isSane = false;
+        return false;
     }
 
+    return true;
+}
+
+function checkParenthesesOrdering(parentheses) {
     let parenthesesStack = [];
-    for (let i = 0; i < parentheses.length; i++) {
-        const token = parentheses[i];
+    for (const token of parentheses) {
         if (token.type === 'openParenthesis') {
             parenthesesStack.push(token);
             continue;
-        } 
-        
+        }
+
         if (token.type === 'closeParenthesis') {
             const lastOpen = parenthesesStack.pop();
             if (!lastOpen) {
                 logError('Parenthesis-ordering is incorrect. At least one closing parenthesis is found without a corresponding opening parenthesis.');
-                isSane = false;
-                break;
+                return false;
             }
         }
     }
+
+    return true;
+}
+
+function checkBracketCountMatches(brackets) {
+    const openBracketCount = brackets.filter((token) => token.type === 'openBracket').length;
+    const closeBracketCount = brackets.filter((token) => token.type === 'closeBracket').length;
 
     if (openBracketCount !== closeBracketCount) {
         logError('Mismatched brackets []');
@@ -54,26 +92,35 @@ export function checkSanity(tokens) {
         if (closeBracketCount > openBracketCount) {
             logError(`Extra ${closeBracketCount - openBracketCount} closing bracket(s)`);
         }
-        isSane = false;
+        return false;
     }
 
+    return true;
+}
+
+function checkBracketOrdering(brackets) {
     let bracketStack = [];
-    for (let i = 0; i < brackets.length; i++) {
-        const token = brackets[i];
+    for (const token of brackets) {
         if (token.type === 'openBracket') {
             bracketStack.push(token);
             continue;
-        } 
-        
+        }
+
         if (token.type === 'closeBracket') {
             const lastOpen = bracketStack.pop();
             if (!lastOpen) {
                 logError('Bracket-ordering is incorrect. At least one closing bracket is found without a corresponding opening bracket.');
-                isSane = false;
-                break;
+                return false;
             }
         }
     }
+
+    return true;
+}
+
+function checkCurlyBraceCountMatches(curlyBraces) {
+    const openCurlyBraceCount = curlyBraces.filter((token) => token.type === 'openCurlyBrace').length;
+    const closeCurlyBracesCount = curlyBraces.filter((token) => token.type === 'closeCurlyBrace').length;
 
     if (openCurlyBraceCount !== closeCurlyBracesCount) {
         logError('Mismatched curly braces {}');
@@ -83,26 +130,35 @@ export function checkSanity(tokens) {
         if (closeCurlyBracesCount > openCurlyBraceCount) {
             logError(`Extra ${closeCurlyBracesCount - openCurlyBraceCount} closing curly brace(s)`);
         }
-        isSane = false;
+        return false;
     }
 
+    return true;
+}
+
+function checkCurlyBraceOrdering(curlyBraces) {
     let curlyBraceStack = [];
-    for (let i = 0; i < curlyBraces.length; i++) {
-        const token = curlyBraces[i];
+    for (const token of curlyBraces) {
         if (token.type === 'openCurlyBrace') {
             curlyBraceStack.push(token);
             continue;
-        } 
-        
+        }
+
         if (token.type === 'closeCurlyBrace') {
             const lastOpen = curlyBraceStack.pop();
             if (!lastOpen) {
                 logError('Curly-brace-ordering is incorrect. At least one closing curly brace is found without a corresponding opening curly brace.');
-                isSane = false;
-                break;
+                return false;
             }
         }
     }
+
+    return true;
+}
+
+function checkConditionalCountMatches(tokens) {
+    const conditionalIfCount = tokens.filter((token) => token.type === 'conditionalIf').length;
+    const conditionalThenCount = tokens.filter((token) => token.type === 'conditionalThen').length;
 
     if (conditionalIfCount !== conditionalThenCount) {
         logError('Mismatched conditional statements (if/then)');
@@ -112,9 +168,13 @@ export function checkSanity(tokens) {
         if (conditionalThenCount > conditionalIfCount) {
             logError(`Extra ${conditionalThenCount - conditionalIfCount} 'then' statement(s)`);
         }
-        isSane = false;
+        return false;
     }
 
+    return true;
+}
+
+function checkOnlyLiteralsBetweenCurlyBraces(tokens) {
     let stack = [];
     for (let i = 0; i < tokens.length; i++) {
         const token = tokens[i];
@@ -123,17 +183,20 @@ export function checkSanity(tokens) {
         } else if (token.type === 'closeCurlyBrace') {
             const lastOpen = stack.pop();
             if (!lastOpen) {
-                logError('Extra closing curly brace found');
-                isSane = false;
+                return false;
             }
         } else if (stack.length > 0 && token.type !== 'literal') {
             logError(`Only literals are allowed between curly braces, found: ${token.type}`);
             logError(`...${tokens.slice(i - 2, i + 3).map((t) => t.value).join(' ')}...`);
-            isSane = false;
+            return false;
         }
     }
 
-    stack = [];
+    return true;
+}
+
+function checkOnlyLiteralsBetweenBrackets(tokens) {
+    let stack = [];
     for (let i = 0; i < tokens.length; i++) {
         const token = tokens[i];
         if (token.type === 'openBracket') {
@@ -141,15 +204,14 @@ export function checkSanity(tokens) {
         } else if (token.type === 'closeBracket') {
             const lastOpen = stack.pop();
             if (!lastOpen) {
-                logError('Extra closing bracket found');
-                isSane = false;
+                return false;
             }
         } else if (stack.length > 0 && token.type !== 'literal') {
             logError(`Only literals are allowed between brackets, found: ${token.type}`);
             logError(`...${tokens.slice(i - 2, i + 3).map((t) => t.value).join(' ')}...`);
-            isSane = false;
+            return false;
         }
-    }
 
-    return isSane;
+        return true;
+    }
 }
